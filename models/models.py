552e01f9cff8313517c8feeff4ff53796d50b6c0
khalilozetch <ahmed_aaa012@yahoo.com>
2019-04-21 20:28:21 +0200

    [Add]sss

--- models/models.py
+++ models/models.py
@@ -4,11 +4,11 @@ from odoo import models, fields, api
 
 
 class CarRequest(models.Model):
-    _name = 'car_requst'  # table in database
+    _name = 'car.request'  # table in database
     _rec_name = 'name'
     _description = 'Car Request'
     name = fields.Char(string="Request", required=True, )
-    date_from = fields.Char(string=" Starting Date ", defult=fields.datetime.now(), )
+    date_from = fields.Char(string=" Starting Date ", default=fields.datetime.now(), )
     date_to = fields.Char(string=" End Date ", required=False, )
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
     car_id = fields.Many2one(comodel_name="fleet.vehicl", string="Car", required=True, )
